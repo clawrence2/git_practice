f0ea9f85efdb327b1d9dd92388a410be72d922e2
Collin Lawrence <hicollin@gmail.com>
2021-12-06 10:41:44 -0600

    added 2 console logs and a comment

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
 console.log("hello world")
+console.log("are you using the diff function for this")
+console.log("at the time this was written, this was an unstaged change")
+//here is yet another change
